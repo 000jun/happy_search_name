54625af525029bac4b04604aa389c5f3165773ac
Choi Youngjun <112798357+000jun@users.noreply.github.com>
2024-05-17 18:20:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ st.header('이름 검색기 페이지', divider='rainbow')
 
 # 데이터프레임 호출
 df = pd.read_excel('secure_data_0517.xlsx')
+df.rename(columns={' 번호':'번호'}, inplace=True)
 df['last_number'] = df['last_number'].astype(str)
 
 # 이름 입력
@@ -17,6 +18,9 @@ name = st.text_input(label='검색할 이름을 입력하시오.')
 # 이름 검색 
 condition = df['이름'] == name
 result = df[condition]
+
+print(result)
+print(result.columns)
 
 # 결과 출력
 st.write(f":orange[{name}]님의 번호는 :orange[{list(result['번호'].values)}]입니다.")
